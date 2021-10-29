6722916ad3d55e4ed61549f6113fafea1f3c0d23
Jasmina <40425222+TBP-BE@users.noreply.github.com>
2021-10-29 11:37:25 +0200

    Delete ClientApplication/scip-client-application directory

--- ClientApplication/scip-client-application/backend/src/main/java/blockchains/iaas/uni/stuttgart/de/scipcasestudy/clientapplication/backend/model/request/ScipRequestMessage.java
+++
@@ -1,9 +0,0 @@
-package blockchains.iaas.uni.stuttgart.de.scipcasestudy.clientapplication.backend.model.request;
-
-import java.io.Serializable;
-
-import lombok.experimental.SuperBuilder;
-
-@SuperBuilder
-public abstract class ScipRequestMessage implements Serializable {
-}
